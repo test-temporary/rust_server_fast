a8abaaf8eebded0be6076d2469a5a5b53eee0627
Musab Gültekin <gultekinmusab@gmail.com>
2024-09-15 23:03:34 +0300

    simplify

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,11 @@ async fn hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let port = std::env::var("PORT").ok().and_then(|p| p.parse().ok()).unwrap_or(8080);
     HttpServer::new(|| {
         App::new()
             .service(hello)
     })
-    .bind(("0.0.0.0", port))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
